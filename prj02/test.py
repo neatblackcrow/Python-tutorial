5c17b8afc7d3abd71aa66caadfa95eedff353189
Voravut Nateluercha <fieldfirst2012@gmail.com>
2013-02-22 23:36:33 +0700

    Finished

--- prj02/test.py
+++ prj02/test.py
@@ -3,31 +3,32 @@ from webshop import Webshop
 from webshop_model import Product, Customer, Order, Products, Customers, Orders
 
 class TestWebshop(unittest.TestCase):
+
     def setUp(self):
         self.webshop = Webshop()
-        
+
     def test_add_customer1(self):
         n = self.webshop.get_total_customers()
         assert self.webshop.add_customer(u'test1@example.com',u'test',u'test') != None
         assert self.webshop.get_total_customers() - n == 1
-        
+
     def test_add_customer2(self):
         n = self.webshop.get_total_customers()
         assert self.webshop.add_customer(u'test1@example.com',u'test',u'test') != None
         assert self.webshop.add_customer(u'test1@example.com',u'test',u'test') == None
         assert self.webshop.get_total_customers() - n == 1
-        
+
     def test_add_product1(self):
         n = self.webshop.get_total_products()
         assert self.webshop.add_product(u'T-0001', u'test', u'test', 0.99) != None
         assert self.webshop.get_total_products() - n == 1
-        
+
     def test_add_product2(self):
         n = self.webshop.get_total_products()
         assert self.webshop.add_product(u'T-0001', u'test', u'test', 0.99) != None
         assert self.webshop.add_product(u'T-0001', u'test', u'test', 0.99) == None
         assert self.webshop.get_total_products() - n == 1
-        
+
     def test_add_order1(self):
         n = self.webshop.get_total_orders()
         customer = self.webshop.add_customer(u'test1@example.com',u'test',u'test')
@@ -40,7 +41,7 @@ class TestWebshop(unittest.TestCase):
         assert len(customer.orders) == 1
         assert len(order.products) == 3
         assert order.customer.email == customer.email
-        
+
     def test_add_order2(self):
         n = self.webshop.get_total_orders()
         customer = self.webshop.add_customer(u'test1@example.com',u'test',u'test')
@@ -48,29 +49,29 @@ class TestWebshop(unittest.TestCase):
         product2 = self.webshop.add_product(u'T-0002', u'test2', u'test', 1.99)
         product3 = self.webshop.add_product(u'T-0003', u'test3', u'test', 2.99)
         assert self.webshop.add_order(u'R-00001', customer, [product1, product2, product3]) != None
-        assert self.webshop.add_order(u'R-00001', customer, [product1, product2, product3]) == None        
+        assert self.webshop.add_order(u'R-00001', customer, [product1, product2, product3]) == None
         assert self.webshop.get_total_orders() - n == 1
-        
+
     def test_delete_customer1(self):
         self.webshop.add_customer(u'test1@example.com',u'test',u'test')
         assert self.webshop.delete_customer(u'test1@example.com') == True
         assert self.webshop.get_total_customers() == 0
-        
+
     def test_delete_customer2(self):
         self.webshop.add_customer(u'test1@example.com',u'test',u'test')
         assert self.webshop.delete_customer(u'test2@example.com') == False
         assert self.webshop.get_total_customers() == 1
-        
+
     def test_delete_product1(self):
         self.webshop.add_product(u'T-0001', u'test', u'test', 0.99)
         assert self.webshop.delete_product(u'T-0001') == True
         assert self.webshop.get_total_products() == 0
-    
+
     def test_delete_product2(self):
         self.webshop.add_product(u'T-0001', u'test', u'test', 0.99)
         assert self.webshop.delete_product(u'T-00002') == False
         assert self.webshop.get_total_products() == 1
-        
+
     def test_delete_order1(self):
         n = self.webshop.get_total_orders()
         customer = self.webshop.add_customer(u'test1@example.com',u'test',u'test')
@@ -91,12 +92,12 @@ class TestWebshop(unittest.TestCase):
         product2 = self.webshop.add_product(u'T-0002', u'test2', u'test', 1.99)
         product3 = self.webshop.add_product(u'T-0003', u'test3', u'test', 2.99)
         self.webshop.add_order(u'R-00001', customer, [product1, product2, product3])
-        self.webshop.add_order(u'R-00002', customer, [product1, product1, product2, product3])        
+        self.webshop.add_order(u'R-00002', customer, [product1, product1, product2, product3])
         assert self.webshop.delete_order(u'R-00003') == False
         assert self.webshop.get_total_orders() == 2
         assert self.webshop.get_total_customers() == 1
         assert self.webshop.get_total_products() == 3
-    
+
     def test_get_products_in_price_range(self):
         self.webshop.add_product(u'T-0001', u'test', u'test', 0.99)
         self.webshop.add_product(u'T-0002', u'test', u'test', 1.99)
@@ -112,11 +113,11 @@ class TestWebshop(unittest.TestCase):
         assert len(self.webshop.get_products_in_price_range(max_price=50)) == 5
         assert len(self.webshop.get_products_in_price_range(max_price=10)) == 2
         assert len(self.webshop.get_products_in_price_range(min_price=30, max_price=400)) == 3
-    
+
     def test_get_best_customers(self):
         customer1 = self.webshop.add_customer(u'test1@example.com',u'test1',u'test')
         customer2 = self.webshop.add_customer(u'test2@example.com',u'test2',u'test')
-        customer3 = self.webshop.add_customer(u'test3@example.com',u'test3',u'test')        
+        customer3 = self.webshop.add_customer(u'test3@example.com',u'test3',u'test')
         product1 = self.webshop.add_product(u'T-0001', u'test1', u'test', 0.99)
         product2 = self.webshop.add_product(u'T-0002', u'test2', u'test', 1.99)
         product3 = self.webshop.add_product(u'T-0003', u'test3', u'test', 2.99)
@@ -130,7 +131,7 @@ class TestWebshop(unittest.TestCase):
         self.webshop.add_order(u'R-00003', customer3, [product1, product2, product1, product2])
         self.webshop.add_order(u'R-00004', customer1, [product8, product2, product1, product2])
         assert self.webshop.get_best_customers()[0].email == u'test1@example.com'
-        
+
     def test_get_orders_of_customer(self):
         customer = self.webshop.add_customer(u'test1@example.com',u'test',u'test')
         product1 = self.webshop.add_product(u'T-0001', u'test1', u'test', 0.99)
@@ -142,13 +143,13 @@ class TestWebshop(unittest.TestCase):
 
         orders = self.webshop.get_orders_of_customer(u'test1@example.com')
         assert len(orders) == 3
-        
+
         order_codes = map(lambda x:x.order_code, orders)
-        order_codes.sort()        
+        order_codes.sort()
         assert order_codes[0] == u'R-00001'
         assert order_codes[1] == u'R-00002'
-        assert order_codes[2] == u'R-00003'                
-        
+        assert order_codes[2] == u'R-00003'
+
     def test_get_customers_who_ordered_product(self):
         customer1 = self.webshop.add_customer(u'test1@example.com',u'test1',u'test1')
         customer2 = self.webshop.add_customer(u'test2@example.com',u'test2',u'test2')
@@ -161,13 +162,13 @@ class TestWebshop(unittest.TestCase):
         self.webshop.add_order(u'R-00003', customer3, [product2, product3])
         customers = self.webshop.get_customers_who_ordered_product(u'T-0001')
         assert len(customers) == 2
-        
+
         emails = map(lambda x:x.email, customers)
         emails.sort()
         assert emails[0] == u'test1@example.com'
-        assert emails[1] == u'test2@example.com'        
-        
-        
+        assert emails[1] == u'test2@example.com'
+
+
     def test_get_cheapest_products(self):
         product1 = self.webshop.add_product(u'T-0001', u'test1', u'test', 0.99)
         product2 = self.webshop.add_product(u'T-0002', u'test2', u'test', 1.99)
@@ -177,13 +178,13 @@ class TestWebshop(unittest.TestCase):
         product6 = self.webshop.add_product(u'T-0006', u'test6', u'test', 100.99)
         product7 = self.webshop.add_product(u'T-0007', u'test7', u'test', 300.99)
         product8 = self.webshop.add_product(u'T-0008', u'test8', u'test', 500.99)
-        
+
         products = self.webshop.get_cheapest_products()
         assert len(products) == 2
         for product in products:
             assert product.price_ex_vat == 0.99
 
-        
+
     def test_get_most_expensive_products(self):
         product1 = self.webshop.add_product(u'T-0001', u'test1', u'test', 0.99)
         product2 = self.webshop.add_product(u'T-0002', u'test2', u'test', 1.99)
@@ -193,12 +194,12 @@ class TestWebshop(unittest.TestCase):
         product6 = self.webshop.add_product(u'T-0006', u'test6', u'test', 200.99)
         product7 = self.webshop.add_product(u'T-0007', u'test7', u'test', 300.99)
         product8 = self.webshop.add_product(u'T-0008', u'test8', u'test', 500.99)
-        
+
         products = self.webshop.get_most_expensive_products()
-        assert len(products) == 1     
+        assert len(products) == 1
         for product in products:
             assert product.price_ex_vat == 500.99
-        
+
     def test_get_top_products(self):
         customer1 = self.webshop.add_customer(u'test1@example.com',u'test1',u'test1')
         customer2 = self.webshop.add_customer(u'test2@example.com',u'test2',u'test2')
@@ -211,7 +212,7 @@ class TestWebshop(unittest.TestCase):
         product6 = self.webshop.add_product(u'T-0006', u'test6', u'test', 200.99)
         product7 = self.webshop.add_product(u'T-0007', u'test7', u'test', 300.99)
         product8 = self.webshop.add_product(u'T-0008', u'test8', u'test', 500.99)
-        
+
         self.webshop.add_order(u'R-00001', customer1, [product1, product2, product3, product4, product7])
         self.webshop.add_order(u'R-00002', customer2, [product1, product2, product8])
         self.webshop.add_order(u'R-00003', customer3, [product2, product3, product5, product8])
@@ -221,16 +222,16 @@ class TestWebshop(unittest.TestCase):
         self.webshop.add_order(u'R-00007', customer1, [product1, product2, product3, product4, product5])
         self.webshop.add_order(u'R-00008', customer2, [product1, product2, product8])
         self.webshop.add_order(u'R-00009', customer3, [product2, product3, product5, product8])
-        
+
         products = self.webshop.get_top_products(4)
         assert len(products) == 4
         skus = map(lambda x:x.sku, products)
         skus.sort()
-        
+
         assert skus[0] == u'T-0001'
         assert skus[1] == u'T-0002'
         assert skus[2] == u'T-0003'
-        assert skus[3] == u'T-0008'                        
-        
+        assert skus[3] == u'T-0008'
+
 if __name__ == '__main__':
     unittest.main()
--- prj02/webshop.py
+++ prj02/webshop.py
@@ -9,44 +9,48 @@ class Webshop:
 
     def get_total_customers(self):
         """ Return total number of customers
-        
+
         @rtype: integer
         """
-        pass
-        
+        return len(self.customers)
+
     def get_total_products(self):
         """ Return total number of products
-        
+
         @rtype: integer
         """
-        pass
+        return len(self.products)
 
     def get_total_orders(self):
         """ Return total number of orders
-        
+
         @rtype: integer
         """
-        pass
+        return len(self.orders)
 
     def add_customer(self, email, first_name, last_name):
         """ Add new customer if he/she does not exist (identified by email)
         Return the new customer if successes otherwise None
-        
+
         @param email: email address
         @type email: unicode string
         @param first_name: first name
         @type first_name: unicode string
         @param last_name: last name
         @type last_name: unicode string
-        
+
         @rtype: Customer
         """
-        pass
+        if email not in self.customers:
+            self.customers.add(email, Customer(email, first_name, last_name))
+            return Customer(email, first_name, last_name)
+        else:
+            return None
 
     def add_product(self, sku, name, description, price_ex_vat):
         """ Add new product if it does not exist (identified by sku)
         Return the new product if successes otherwise None
-        
+
         @param sku: SKU
         @type sku: unicode string
         @param name: name
@@ -55,115 +59,178 @@ class Webshop:
         @type description: unicode string
         @param price_ex_vat: price excluding vat
         @type price_ex_vat: float
-        
+
         @rtype: Product
         """
-        pass
+        if sku not in self.products:
+            self.products.add(sku, Product(sku, name, description, price_ex_vat))
+            return  Product(sku,name, description, price_ex_vat)
+        else:
+            return None
 
     def add_order(self, order_code, customer, products):
         """ Add new order if it does not exist (identified by order_code)
         Return the new order if successes otherwise None
-        
+
         @param order_code: unique code of each product
         @type order_code: unicode string
         @param customer: customer
         @type customer: Customer
         @param products: products
         @type products: list of Product
-        
+
         @rtype: Order
-        """     
-        pass
-        
+        """
+        if order_code not in self.orders:
+            customer.add_order(Order(order_code, customer, products))
+            self.customers[customer.email].add_order(Order(order_code, customer, products))
+            self.orders.add(order_code, Order(order_code, customer, products))
+            return Order(order_code, customer, products)
+        else:
+            return None
+
     def delete_customer(self, email):
         """ Delete the customer who has the given email
         Return True if successes otherwise False
-        
+
         @param email: email address
         @type email: unicode string
-        
+
         @rtype: boolean
         """
-        pass
-            
+        if email in self.customers:
+            self.customers.remove(email)
+            return True
+        else:
+            return False
+
     def delete_product(self, sku):
         """ Delete the product which has the given sku
         Return True if successes otherwise False
 
         @param sku: SKU
         @type sku: unicode string
-        
+
         @rtype: boolean
         """
-        pass
-        
+        if sku in self.products:
+            self.products.remove(sku)
+            return True
+        else:
+            return False
+
     def delete_order(self, order_code):
         """ Delete the order which has the given order_code
         Return True if successes otherwise False
 
         @param order_code: unique code of each product
         @type order: unicode string
-        
-        @rtype: boolean        
+
+        @rtype: boolean
         """
-        pass
+        if order_code in self.orders:
+            self.orders.remove(order_code)
+            return True
+        else:
+            return False
 
     def get_products_in_price_range(self, min_price=None, max_price=None):
         """ Get products in the given range of price
 
         @rtype: list of Product
         """
-        pass
+        result = []
+        if max_price == None:
+            max_price = max(self.products.get_product_prices())
+        for product in self.products.values():
+            if (min_price <= product.price_ex_vat <= max_price):
+                result.append(product)
+        return result
 
     def get_best_customers(self):
         """ Return customers that have highest total price of orders
 
         @rtype: list of Customer
         """
-        pass
+        result = []
+        temp_dict = {}
+        for customer in self.customers.values():
+            for order in customer.orders:
+                if customer.email not in temp_dict.keys():
+                    temp_dict[customer.email] = self.orders.total_prices_from_order(order)
+                else:
+                    temp_dict[customer.email] += self.orders.total_prices_from_order(order)
+        max_value = max([price for price in temp_dict.values()])
+        for email, price_count in temp_dict.items():
+            if price_count == max_value:
+                result.append(self.customers[email])
+        return result
 
     def get_orders_of_customer(self, email):
         """ Get orders of the given customer
-        
+
         @param email: email address
         @type email: unicode string
 
         @rtype: list of Order
         """
-        pass
-        
+        return self.customers[email].orders
+
     def get_customers_who_ordered_product(self, sku):
         """ Get customers who ordered the given product
-        
+
         @param sku: SKU
         @type sku: unicode string
-        
+
         @rtype: list of Customer
-        """        
-        pass
-        
+        """
+        result = []
+        for order in self.orders.values():
+            for product in order.products:
+                if product.sku == sku:
+                    result.append(order.customer)
+        return result
+
     def get_cheapest_products(self):
         """ Return the cheapest products in the shop
-        
+
         @rtype: list of Product
         """
-        pass
-        
+        min_price = min(self.products.get_product_prices())
+        min_price_product = self.products.get_products_from_price(min_price)
+        return min_price_product
+
     def get_most_expensive_products(self):
         """ Return the most expensive products in the shop
-        
+
         @rtype: list of Product
         """
-        pass
-        
-    def get_top_products(self, n):        
-        """ Return n products that is ordered highest
-        
+        max_price = max(self.products.get_product_prices())
+        max_price_product = self.products.get_products_from_price(max_price)
+        return max_price_product
+
+    def get_top_products(self, n):
+        """ Return products that is ordered highest
+
         @param n: number of products
         @type n: integer
-        
+
         @rtype: list of Product
-        """        
-        pass
-        
-    
+        """
+        temp_dict = {}
+        result = []
+        for order in self.orders.values():
+            for product in self.orders.get_products_from_order(order):
+                if product.sku not in temp_dict:
+                    temp_dict[product.sku] = 1
+                else:
+                    temp_dict[product.sku] += 1
+        i = 1
+        while i <= n:
+            max_value = max([item for item in temp_dict.values()])
+            for key, value in temp_dict.items():
+                if value == max_value:
+                    result.append(self.products[key])
+                    del temp_dict[key]
+                    i += 1
+        return result
